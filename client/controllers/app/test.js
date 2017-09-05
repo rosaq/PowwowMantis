6a566b4ca74b70244bd2479a4b2cebd72bcad8fe
Rosa Quintero <rosa.quintero@flex.com>
2017-09-05 08:54:42 -0700

    [Studio] Add screen "test"

---
+++ client/controllers/app/test.js
@@ -0,0 +1,5 @@
+angular.module('app').controller('app_test', app_test);
+function app_test($scope, app) {
+    'use strict';
+    app.init($scope);
+}
--- client/states.js
+++ client/states.js
@@ -42,5 +42,14 @@ angular.module('app').config(function ($stateProvider) {
                 }
             }
         }
+    }).state('app.test', {
+        views: {
+            app: {
+                controller: 'app_test',
+                templateProvider: function (app) {
+                    return app.templateProvider('app.test');
+                }
+            }
+        }
     });
 });
