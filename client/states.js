1f6d9b5e49afaf9041a36d6d20cb0979127bb5a1
Rosa Quintero <rosa.quintero@flex.com>
2017-09-05 09:17:54 -0700

    [Studio] Add screen "viewissues"

--- client/states.js
+++ client/states.js
@@ -51,5 +51,14 @@ angular.module('app').config(function ($stateProvider) {
                 }
             }
         }
+    }).state('app.viewissues', {
+        views: {
+            app: {
+                controller: 'app_viewissues',
+                templateProvider: function (app) {
+                    return app.templateProvider('app.viewissues');
+                }
+            }
+        }
     });
 });
